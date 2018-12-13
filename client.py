976a44f8dce755e9c63efbc78c0d79fd49b1a3dd
Almir M. da Cunha <almirmdacunha@gmail.com>
2018-12-13 16:23:14 -0300

    Send client data to server

--- client.py
+++ client.py
@@ -1,12 +1,25 @@
-from socket import *
+from socket import socket, AF_INET, SOCK_DGRAM, timeout
+from common import ip_checksum
 
 class Client:
-    clientSocket = socket(AF_INET,SOCK_DGRAM)
+    send_socket = socket(AF_INET,SOCK_DGRAM)
+    recv_socket = socket(AF_INET,SOCK_DGRAM)
+    seq = 0
 
-    def bind(cli_addr, cli_port): # Bind client socket
-        clientSocket.bind((cli_addr, cli_port))
+    def bind(self, cli_addr, cli_port): # Bind the client receiver socket
+        self.recv_socket.bind((cli_addr, cli_port))
+        self.recv_socket.settimeout(1)
     
-    def send(data, server_addr, server_port): # Send data to server
-        pass
-
-
+    def send(self, data, server_addr, server_port): # Send data to server
+        ack_received = False
+        dest = (server_addr, server_port)
+        while not ack_received:
+            send_socket.sendto(ip_checksum(data) + str(self.seq) + data, dest)
+            try:
+                message, address = recv_socket.recvfrom(4096)
+            except timeout:
+                pass
+            else:
+                if ip_checksum(message[2:0]) == message[:2] and message[5] == str(self.seq):
+                    ack_received = True
+        self.seq = 1-self.seq
